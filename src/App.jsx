9e1905aec3bff63b9a9e4084bd2f2c5c471afa5f
LauraNorah <laura.nora.azevedo@gmail.com>
2025-05-30 12:09:34 -0300

    meu primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import UserCard from './components/UserCard';
+import './App.css';
+
+function App() {
+  const [users, SetUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/peoples')
+      .then((res) => res.json())
+      .then((data) => SetUsers(data))
+      .catch((err) => console.error('Erro ao buscar usuários:', err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Dashboard de Usuários</h1>
+      <p>Total de usuários: {users.length}</p>
+      <div className="user-container">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
